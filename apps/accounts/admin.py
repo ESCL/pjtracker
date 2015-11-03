427a6c9fcc6b1f6b45efa53c1fd1a15bedc706cd
kako <claudio.melendrez@gmail.com>
2015-11-03 19:35:08 -0300

    Fixed admin user password issue

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 from .models import Account, UserProfile
@@ -13,10 +14,10 @@ class UserProfileInline(admin.StackedInline):
     can_delete = False
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserWithProfileAdmin(UserAdmin):
     inlines = (UserProfileInline,)
 
 
 admin.site.register(Account, AccountAdmin)
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, UserWithProfileAdmin)
