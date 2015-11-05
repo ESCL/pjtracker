6b108c6d79e470f5207cb59d94bd0e363b942514
kako <claudio.melendrez@gmail.com>
2015-11-05 11:38:06 -0300

    Added login and logout views

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,10 @@
 __author__ = 'kako'
 
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    url(r'^login/', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
 ]
