e1a994f0bbf2bf9dd90269aa17f1e7e64aec1bea
kako <claudio.melendrez@gmail.com>
2015-11-10 04:58:37 -0300

    Added promise-based ajax utility functions

---
+++ apps/common/static/js/ajax.js
@@ -0,0 +1,73 @@
+/**
+ * Created by kako on 10/11/15.
+ *
+ * A few utility functions to use promises so we don't need
+ * jQuery nor callbacks.
+ *
+ */
+
+function getCsrfToken() {
+    // Get CSRF token, used by Django forms
+    return document.cookie.match(/csrftoken=(\w+)/)[1];
+}
+
+function wait() {
+    // Placeholder call used as dummy AJAX call
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve('done');
+        }, 5000);
+    });
+}
+
+function get(url, querystring) {
+    return new Promise(function(resolve, reject) {
+        // Open xhr and set headers
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url + "?" + querystring, true);
+
+        // Handle onload, resolve on status 201, reject on others
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                resolve(xhr.response);
+            } else {
+                reject(Error(xhr.statusText));
+            }
+        };
+
+        // Right, network errors are possible as well...
+        xhr.onerror = function() {
+            reject(Error("Connection Error"))
+        };
+
+        // Finally, send the stuff
+        xhr.send(null);
+    });
+}
+
+function post(url, data) {
+    return new Promise(function(resolve, reject) {
+        // Open xhr and set headers
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.setRequestHeader("X-CSRFToken", getCsrfToken());
+
+        // Handle onload, resolve on status 201, reject on others
+        xhr.onload = function() {
+            if (xhr.status == 201) {
+                resolve(xhr.response);
+            } else {
+                reject(Error(xhr.statusText));
+            }
+        };
+
+        // Right, network errors are possible as well...
+        xhr.onerror = function() {
+            reject(Error("Connection Error"))
+        };
+
+        // Finally, send the stuff
+        xhr.send(data);
+    });
+}
