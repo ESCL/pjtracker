551770bc12a4ddc7f91454a61fd3b4da078d5a1f
kako <claudio.melendrez@gmail.com>
2015-11-25 00:33:17 -0300

    Added pgsql to reqs and modified prod database to use local pgsql

--- tracker/settings/prod.py
+++ tracker/settings/prod.py
@@ -7,6 +7,17 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+
+DATABASES['default'].update({
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'HOST': 'localhost',
+    'PORT': 5432,
+    'NAME': 'pjtracker',
+    'USER': 'pjtracker',
+    'PASSWORD': 'pjtracker'
+})
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 BOOTSTRAP_EXAMPLE_ACCOUNT = False
