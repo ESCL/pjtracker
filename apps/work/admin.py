433f3d2be519bf7c746b823eed93c164646560a6
kako <claudio.melendrez@gmail.com>
2016-03-12 18:36:52 -0300

    Added missing admins and missing fields in existing ones

--- apps/work/admin.py
+++ apps/work/admin.py
@@ -1,25 +1,30 @@
 from django.contrib import admin
 
-from .models import Project, Activity, ActivityGroup, ActivityGroupType
+from .models import Project, Activity, ActivityGroup, ActivityGroupType, LabourType
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code',)
+    list_display = ('owner', 'code', 'name',)
 
 
 class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'wbs_code', 'project',)
+    list_display = ('owner', 'wbs_code', 'name', 'project',)
 
 
 class ActivityGroupAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code',)
+    list_display = ('owner', 'type', 'code', 'name',)
 
 
 class ActivityGroupTypeAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('owner', 'name',)
+
+
+class LabourTypeAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'code', 'name')
 
 
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Activity, ActivityAdmin)
 admin.site.register(ActivityGroup, ActivityGroupAdmin)
 admin.site.register(ActivityGroupType, ActivityGroupTypeAdmin)
+admin.site.register(LabourType, LabourTypeAdmin)
