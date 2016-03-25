0c5e4174d813352413be73647d38a54982ba5e5b
kako <claudio.melendrez@gmail.com>
2016-03-25 18:16:04 -0300

    Attempt to fix sonar exclusions

--- apps/common/forms/fields.py
+++ apps/common/forms/fields.py
@@ -8,9 +8,9 @@ class CustomLabelModelChoiceField(ModelChoiceField):
     ModelChoice field that allows defining the attribute to use for the
     options labels.
     """
-    def __init__(self, *args, option_label_attr=None, **kwargs):
+    def __init__(self, *args, **kwargs):
+        self.option_label_attr = kwargs.pop('option_label_attr')
         super(CustomLabelModelChoiceField, self).__init__(*args, **kwargs)
-        self.option_label_attr = option_label_attr
 
     def label_from_instance(self, obj):
         return getattr(obj, self.option_label_attr)
