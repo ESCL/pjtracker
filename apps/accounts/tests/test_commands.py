0fba74f3e273502067627fde242b272b66f3a8d3
kako <claudio.melendrez@gmail.com>
2016-06-27 20:41:21 -0300

    Update create_example_account test

--- apps/accounts/tests/test_commands.py
+++ apps/accounts/tests/test_commands.py
@@ -17,15 +17,16 @@ class CreteExampleAccountTest(TestCase):
 
         # Check number of accounts and users added
         self.assertEqual(Account.objects.count(), n_accounts + 1)
-        self.assertEqual(User.objects.count(), n_users + 6)
+        self.assertEqual(User.objects.count(), n_users + 7)
 
         # Check their data
         acc = Account.objects.last()
-        self.assertEqual(User.objects.filter(owner=acc).count(), 6)
-        u1, u2, u3, u4, u5, u6 = User.objects.all()[n_users:]
+        self.assertEqual(User.objects.filter(owner=acc).count(), 7)
+        u1, u2, u3, u4, u5, u6, u7 = User.objects.all()[n_users:]
         self.assertEqual(u1.groups.all()[0].name, 'Administrators')
         self.assertEqual(u2.groups.all()[0].name, 'Human Resources')
         self.assertEqual(u3.groups.all()[0].name, 'Team Management')
         self.assertEqual(u4.groups.all()[0].name, 'Project Control')
-        self.assertEqual(u5.groups.all()[0].name, 'Time-Keeping')
-        self.assertEqual(u6.groups.all()[0].name, 'Supervision')
+        self.assertEqual(u5.groups.all()[0].name, 'Project Management')
+        self.assertEqual(u6.groups.all()[0].name, 'Time-Keeping')
+        self.assertEqual(u7.groups.all()[0].name, 'Supervision')
