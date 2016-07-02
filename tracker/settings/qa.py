847a9ac22a259d2a5e09c71396b36ec6b7684482
kako <claudio.melendrez@gmail.com>
2016-07-02 15:52:03 -0300

    Add QA settings

---
+++ tracker/settings/qa.py
@@ -0,0 +1,60 @@
+__author__ = 'kako'
+
+# Import defaults
+
+from .base import *
+
+
+# Debug level
+
+DEBUG = False
+TASTYPIE_FULL_DEBUG = False
+
+
+# Request listener
+
+ALLOWED_HOSTS = ['*']
+
+
+# CORS allowed domains
+
+CORS_ORIGIN_WHITELIST = (
+    'forecast.escng.com',
+)
+
+
+# Database configuration
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', 5432),
+        'NAME': os.environ.get('DB_NAME', 'pjtracker'),
+        'USER': os.environ.get('DB_USER', 'pjtracker'),
+        'PASSWORD': os.environ.get('DB_PASS', 'pjtracker'),
+    }
+}
+
+
+# Database bootstrapping options
+
+BOOTSTRAP_EXAMPLE_ACCOUNT = False
+
+
+# Static media
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Logging handlers
+
+LOGGING['handlers'] = {
+    'file': {
+        'class': 'logging.FileHandler',
+        'formatter': 'verbose',
+        'filename': '/var/log/django/pjtracker.log',
+    }
+}
+
+LOGGING['loggers']['django']['handlers'] = ['file']
